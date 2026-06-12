8be69f42d730bb073721a354b1b059e83379fa95
arijit-bit <arijitpatra204@gmail.com>
2026-06-12 11:00:36 +0530

    feat: setup root layout with theme provider and implement login page UI

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "../components/theme-provider";
+import { ThemeProvider } from "../components/theme-provider.js";
 import "./globals.css";
 
 const geistSans = Geist({
--- components/theme-provider.js
+++ components/theme-provider.js
@@ -1,8 +1,5 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-
-export function ThemeProvider({ children, ...props }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }) {
+  return children;
 }
